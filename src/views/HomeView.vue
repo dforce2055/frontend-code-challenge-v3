3c1a617bd245b77fb9c32f9b5ffccd8d97540d3b
Diego Pérez <dperez2055@gmail.com>
2024-08-25 18:15:09 -0300

    Removing unused methods

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onBeforeMount, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { useAppStore, useNotificationsStore } from '../store'
 import { getCharacters, type CharacterFilterParams } from '../api/index'
 import type { Character } from '@/types'
